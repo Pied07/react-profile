ef899725aa93e43201851d72c6adfc390defeb87
Pied07 <sandipanadhikary01@gmail.com>
2025-03-28 01:08:13 +0530

    ap.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './components/Home/Home'
 import About from './components/About/About'
