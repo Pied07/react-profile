d7d3f8a86e05f83acb88552147e4a1d9b6b0e4d0
Pied07 <sandipanadhikary01@gmail.com>
2025-03-30 22:48:41 +0530

    contact form done

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import emailjs from '@emailjs/browser'
+import { Link } from 'react-router-dom'
 
 function Contacts() {
     const [formData, setFormData] = useState({ name: '', email: '', subject: '', message: '', attachment: null })
@@ -19,7 +20,14 @@ function Contacts() {
         e.preventDefault();
 
         try {
+            let button = document.querySelector('.formSubmitBtn')
+            button.innerText = "Sending..."
+            button.disabled = true
+            button.style.cursor = "not-allowed"
             await emailjs.sendForm('service_l7u1fki', 'template_b8u3f33', e.target, '0THP0IQVaxvRQvv9V')
+            button.innerText = "Submit"
+            button.disabled = false
+            button.style.cursor = "pointer"
             alert(`Thank You ${formData['name']}, Your Message Has been Submitted Successfully!!!`)
         } catch (error) {
             alert('Error Sending Message!!!')
@@ -30,24 +38,24 @@ function Contacts() {
         <div className="contactContainer">
             <h1>Contact Me</h1>
             <div className="contactItems">
-                <div className="cardContainer">
-                    <div className="cardTitle"><FontAwesomeIcon icon={faPhone} /></div>
-                    <div className="cardElement">
-                        <p>My Contact Number: <a href="tel:+919733413928">Call Me</a></p>
-                    </div>
-                </div>
-                <div className="cardContainer">
+                <Link to="tel:+919733413928" className="cardContainer">
+                        <div className="cardTitle"><FontAwesomeIcon icon={faPhone} /></div>
+                        <div className="cardElement">
+                            <p>Contact Me</p>
+                        </div>
+                </Link>
+                <Link to='mailto:sandipanadhikary01@gmail.com' className="cardContainer">
                     <div className="cardTitle"><FontAwesomeIcon icon={faEnvelope} /></div>
                     <div className="cardElement">
-                        <p>My Email Account: <a href="mailto:sandipanadhikary01@gmail.com">Mail Me</a></p>
+                        <p>Email Me</p>
                     </div>
-                </div>
-                <div className="cardContainer">
+                </Link>
+                <Link to='https://www.linkedin.com/in/sandipan-adhikary-ab418b230/' target='_blank' className="cardContainer">
                     <div className="cardTitle"><FontAwesomeIcon icon={faLinkedin} /></div>
                     <div className="cardElement">
-                        <p>Check my LinkedIn Profile <a href="">Here</a></p>
+                        <p>LinkedIn Profile</p>
                     </div>
-                </div>
+                </Link>
             </div>
             <div className='contactFormContainer'>
                 <h2>Want to Contact me directly from website? Please write yout message here...</h2><br />
@@ -56,9 +64,7 @@ function Contacts() {
                     <input type="email" name='email' placeholder='Your Email...' value={formData.email} onChange={handleChange} required />
                     <input type="text" name="subject" id="subject" placeholder='Your Subject...' onChange={handleChange} required />
                     <textarea name="message" id="message" placeholder='Type Your Message here...' value={formData.message} onChange={handleChange} required />
-                    <label htmlFor="attachment">Add Any File</label>
-                    <input className='attachment' type="file" name='attachment' id='attachment' onChange={handleChange} />
-                    <button>Submit</button>
+                    <button className='formSubmitBtn'>Submit</button>
                 </form>
             </div>
         </div>
